1eea1b33826ea783f81b86e0d29e0ed2413daccd
Ashutosh Tiwari <ashutiwa@iu.edu>
2022-06-08 12:50:55 -0400

    changed dataset location

--- driver.py
+++ driver.py
@@ -35,7 +35,7 @@ import numpy as np
 #
 # weat_scores = weat.get_scores()
 # print(weat_scores)
-ds = Dataset('/tmp/swwiki-latest-pages-articles.xml.bz2')
+ds = Dataset('../swwiki-latest-pages-articles.xml.bz2')
 # print(ds.lines)
 jk = JackKnife(ds)
 scores = jk.weat_scores()
