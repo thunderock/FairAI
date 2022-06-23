c029c73c55265847dd9b0d0dd415d2c9eb292f04
Ashutosh Tiwari <ashutiwa@iu.edu>
2022-06-23 15:51:02 -0400

    need to test

--- models/glove.py
+++ models/glove.py
@@ -7,11 +7,24 @@ import numpy as np
 import scipy.sparse
 import struct
 
+INT_INF = np.iinfo(np.int32).max
+INT_MIN = np.iinfo(np.int32).min
+
 from models.model import Model
+
+class GloveWrapper(object):
+    def __init__(self):
+        self.vocab, self.ivocab, self.W, self.b_w, self.U, self.b_u, self.D, self.d, self.vocab_path, self.embedding_path = [None] * 10
+
 
 class Glove(Model):
     def __init__(self):
         pass
+    #
+    # def non_zero_values_and_indices(self, X:scipy.sparse.csc_matrix, row_index:int=INT_INF, col_index:int=INT_INF) -> (np.array, np.array):
+    #     values = X[row_index if row_index != INT_INF else , col_index].nonzero()[0]
+
+
 
     def load_bin_vectors(self, embedding_path, vocab_size):
         n = os.path.getsize(embedding_path) // 8
@@ -39,7 +52,9 @@ class Glove(Model):
         V = len(vocab)
         W, b_w, U, b_u = self.load_bin_vectors(embedding_path, V)
         D = W.shape[1]
-        return vocab, ivocab, W, b_w, U, b_u, D, d, vocab_path, embedding_path
+        model = GloveWrapper()
+        model.vocab, model.ivocab, model.W, model.b_w, model.U, model.b_u, model.D, model.d, model.vocab_path, model.embedding_path = vocab, ivocab, W, b_w, U, b_u, D, d, vocab_path, embedding_path
+        return model
 
     def load_vocab(self, path):
         str2idx, idx2str = dict(), []
@@ -99,16 +114,62 @@ class Glove(Model):
         I, J, vals = self.parse_coocs(text, vocab, window_size)
         return scipy.sparse.csc_matrix((vals, (I, J)), shape=(V, V))
 
-    def Li(self, W, b_w, U, b_u, X, i):
-        assert len(U.shape) == 2
-        V, D = U.shape
-        Xi = X[i, :].
+    def del_LI(self, W:np.array, b_w:np.array, U:np.array, b_u:np.array, X:scipy.sparse.csc_matrix, idx)->np.array:
+        non_zero_cols = X[idx, :].nonzero()[1]
+        Xi, Ji = X[idx, non_zero_cols].toarray()[0], non_zero_cols
+        diff = U[Ji, :] @ W[idx, :] + b_u[Ji] + b_w[idx] - np.log(Xi)
+        return ((2 * np.vectorize(self.f)(Xi) * diff).reshape(-1, 1).T @ U[Ji, :])[0]
+
+    def del_sq_LI(self, U:np.array, X:scipy.sparse.csc_matrix, idx)->np.array:
+        non_zero_cols = X[idx, :].nonzero()[1]
+        Xi, Ji = X[idx, non_zero_cols].toarray()[0], non_zero_cols
+        d = U.shape[1]
+        temp = np.empty((Xi.shape[0], d))
+        # couldn't figure out alternative of view()
+        for i in range(d):
+            temp[:, i] = np.sqrt(2. * np.vectorize(self.f)(Xi)) * U[Ji, i]
+        return temp.T @ temp
+
     def f(self, x:np.float64, mx:np.float64=100., alpha:np.float64=0.75) -> np.float64:
         if x > mx:
             return np.float64(1.)
         return np.float64((x / mx) ** alpha)
 
-    def compute_IF_deltas(self, document, vocab, ):
+    def compute_IF_deltas(self, document: str, M:GloveWrapper, X:scipy.sparse.csc_matrix):
+        Y = self.doc2cooc(document, M.vocab, M.d)
+        affected_inds = np.unique(Y.nonzero()[0])
+        N = len(affected_inds)
+        deltas = {}
+        if N != 0:
+            # TODO(ashutiwa): make this more efficient here https://seanlaw.github.io/2019/02/27/set-values-in-sparse-matrix/
+            X_bar = X - Y
+            X_bar[X_bar < 0] = 0
+            X_bar.eliminate_zeros()
+            for idx in affected_inds:
+                gi = self.del_LI(M.W, M.b_w, M.U, M.b_u, X, idx)
+                Hi = self.del_sq_LI(M.U, X_bar, idx)
+                g_bar_i = self.del_LI(M.W, M.b_w, M.U, M.b_u, X_bar, idx)
+                deltas[idx] = np.linalg.inv(Hi) @ (gi - g_bar_i)
+        return deltas
+
+    def inv_hessians_for(self, target_indices:np.array, M:GloveWrapper, X:scipy.sparse.csc_matrix):
+        num_words = len(target_indices)
+        H = {}
+        for idx in target_indices:
+            H[idx] = np.linalg.inv(self.del_sq_LI(M.U, X, idx))
+        return H
+
+    def gradients_for(self, target_indices:np.array, M:GloveWrapper, X:scipy.sparse.csc_matrix):
+        num_words = len(target_indices)
+        G = {}
+        for idx in target_indices:
+            G[idx] = self.del_LI(M.W, M.b_w, M.U, M.b_u, X, idx)
+        return G
+
+    def compute_IF_deltas_for(self, document, M:GloveWrapper, X:scipy.sparse.csc_matrix, target_indices:np.array, H:dict, G:dict):
+        num_words = len(target_indices)
+        deltas = {}
+        if num_words != 0:
 # "Model loading"
 # In [1]: from models import glove
 #
